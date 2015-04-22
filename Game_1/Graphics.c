3a2ad09066ea8cd54c60a0bbca683acb4dd8dfd7
Captain593 <ccaptain45@gmail.com>
2015-04-21 22:12:41 -0400

    Patch 1.2
    Implemented some of meditate function, got rid of simplistic dual
    wielding for slightly less-simplistic version.

--- Game_1/Graphics.c
+++ Game_1/Graphics.c
@@ -91,7 +91,7 @@ HANDLE wHnd;    // Handle to write to the console.
 HANDLE rHnd;    // Handle to read from the console.
 AnimationPtr boxes;
 
-int main(int argc, char* argv[]){
+int main2(int argc, char* argv[]){
 	int i, j, k;
 	Player mainChar = malloc(sizeof(PlayerSize));
 	Enemy enemies = malloc(sizeof(EnemySize));
